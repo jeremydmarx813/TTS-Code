0cf4cf9e13c1898b1388d869f8a49e5a35030f11
jeremydmarx <jeremydmarx@gmail.com>
2020-03-24 16:40:02 -0500

    fixed scoreboard issue, increased round number threshold, optimized for multiplayer, but not syncing up with dom

--- rockPaperHW31220/script.js
+++ rockPaperHW31220/script.js
@@ -30,136 +30,152 @@ var player4 = {
 };
 
 var scoreBoard = {
-	playerOneWins   : player1.wins,
-	playerTwoWins   : player2.wins,
-	playerThreeWins : player3.wins,
-	playerFourWins  : player4.wins
+	// playerOneWins   : player1.wins,
+	// playerTwoWins   : player2.wins,
+	// playerThreeWins : player3.wins,
+	// playerFourWins  : player4.wins
 };
-// var rock = 0;
-// var paper = 1;
-// var scissors = 2;
 
-// var player1Toss;
-// var player2Toss;
-// var player1Wins = 0;
-// var player2Wins = 0;
-
-// console.log(document.getElementsByClassName('player-score-number'));
+var currentPlayerA = document.getElementById('playerAMenu').value;
+var currentPlayerB = document.getElementById('playerBMenu').value;
 
 console.group('%cstart new round!', 'font-size: 14px; color: red;');
+currentPlayerChecker();
+
+function currentPlayerChecker() {
+	console.log('Player A is: ', currentPlayerA);
+	console.log('Player B is: ', currentPlayerB);
+}
+
 function assignToss(playerA, playerB) {
+	console.count('Round Number');
+	console.log(typeof playerA);
 	function generateToss() {
 		return parseInt(Math.random() * 10) % 3;
 	}
 	// var objNum = [ 'rock', 'paper', 'scissors' ];
-	[ playerA ].currentToss = hands[generateToss()];
+	playerA.currentToss = hands[generateToss()];
 	console.log(playerA);
-	[ playerB ].currentToss = hands[generateToss()];
+	playerB.currentToss = hands[generateToss()];
 	console.log(playerB);
-	console.log('Player1 throws a ' + [ playerA ].curentToss + ', Player2 throws a ' + [ playerB ].curentToss);
-	playerThrows.innerHTML =
-		'Player1 throws a ' + [ playerA ].curentToss + ', Player2 throws a ' + [ playerB ].curentToss;
+	console.log('Player1 throws a ' + playerA.currentToss + ', Player2 throws a ' + playerB.currentToss);
+	playerThrows.innerHTML = 'Player1 throws a ' + playerA.currentToss + ', Player2 throws a ' + playerB.currentToss;
 	return;
 }
 
 function throwDown(playerA, playerB) {
-	var winner;
-	if ([ playerA ].currentToss === [ playerB ].currentToss) {
+	// var winner;
+	if (playerA.currentToss === playerB.currentToss) {
 		winnerText.innerHTML = 'Tie, throw down again!';
 		console.log('Tie, throw down again!');
-	} else if ([ playerA ].currentToss === 'rock') {
-		if ([ playerB ].currentToss === 'paper') {
+	} else if (playerA.currentToss === 'rock') {
+		if (playerB.currentToss === 'paper') {
 			winner = 'Player 2';
 			winnerText.innerHTML = 'Player 2 wins';
 			console.log('Player 2 wins!');
-			[ playerB ].wins++;
+			playerB.wins++;
 		} else {
 			winner = 'Player 1';
 			winnerText.innerHTML = 'Player 1 wins';
 			console.log('Player 1 wins!');
-			[ playerA ].wins++;
+			playerA.wins++;
 		}
-	} else if ([ playerA ].curentToss === 'paper') {
-		if ([ playerB ].currentToss === 'scissors') {
+	} else if (playerA.currentToss === 'paper') {
+		if (playerB.currentToss === 'scissors') {
 			winner = 'Player 2';
 			winnerText.innerHTML = 'Player 2 wins';
 			console.log('Player 2 wins!');
-			[ playerB ].wins++;
+			playerB.wins++;
 		} else {
 			winner = 'Player 1';
 			winnerText.innerHTML = 'Player 1 wins';
 			console.log('Player 1 wins!');
-			[ playerA ].wins++;
+			playerA.wins++;
 		}
-	} else if ([ playerA ].currentToss === 'scissors') {
-		if ([ playerB ].currentToss === 'rock') {
+	} else if (playerA.currentToss === 'scissors') {
+		if (playerB.currentToss === 'rock') {
 			winner = 'Player 2';
 			winnerText.innerHTML = 'Player 2 wins';
 			console.log('Player 2 wins!');
-			[ playerB ].wins++;
+			playerB.wins++;
 		} else {
 			winner = 'Player 1';
 			winnerText.innerHTML = 'Player 1 wins';
 			console.log('Player 1 wins!');
-			[ playerA ].wins++;
+			playerA.wins++;
 		}
 	}
 	// console.log(winner);
-	return winner;
+	// return winner;
+}
+//??Why is the function necessary to keep the scoreBoard up to date?
+function updateScoreBoard(board) {
+	board.playerOneWins = player1.wins;
+	board.playerTwoWins = player2.wins;
+	board.playerThreeWins = player3.wins;
+	board.playerFourWins = player4.wins;
+	return board;
 }
 
 function scoreKeeper() {
-	player1ScoreNumber.innerHTML = [ player1 ].wins;
-	player2ScoreNumber.innerHTML = [ player2 ].wins;
-	console.log('SCORE: Player1: ' + [ player1 ].wins + '. Player2: ' + [ player2 ].wins + '.');
+	player1ScoreNumber.innerHTML = player1.wins;
+	player2ScoreNumber.innerHTML = player2.wins;
+	console.log('SCORE: Player1: ' + player1.wins + '. Player2: ' + player2.wins + '.');
+	console.log('scoreboard', scoreBoard);
 }
 
 function winnerChecker(a, b) {
-	console.count('Round Number');
-	if (a === 3 || b === 3) {
+	if (a === 5 || b === 5) {
 		playButton.innerText = 'Game over!';
 		playButton.onclick = resetGame;
-		if (a === 3) {
+		if (a === 5) {
 			console.log('Player 1 Wins the Whole Match!!');
+			console.log('player 1', player1);
 			console.log('score board', scoreBoard);
 			winnerText.innerText = 'Player 1 Wins the Whole Match!!';
-			winner = Array.from(document.getElementById('player1-score').children);
-			document.getElementById('player1-score').children[0].style.cssText = 'color: green';
-			player1ScoreNumber.style.cssText = 'color: green';
+			winner = Array.from(document.getElementById('playerA-score').children)[1];
+			winner.style.cssText = 'color: green';
+			// player1ScoreNumber.style.cssText = 'color: green';
 		} else {
 			console.log('Player 2 Wins the Whole Match!!');
+			console.log('player 2 log', player2);
 			console.log('score board', scoreBoard);
 			winnerText.innerText = 'Player 2 Wins the Whole Match!!';
-			winner = Array.from(document.getElementById('player2-score').children);
-			document.getElementById('player2-score').children[0].style.cssText = 'color: green';
-			player2ScoreNumber.style.cssText = 'color: green';
+			winner = Array.from(document.getElementById('playerB-score').children)[1];
+			winner.style.cssText = 'color: green';
+			// player2ScoreNumber.style.cssText = 'color: green';
 		}
 	}
 }
 
 function gamePlayer() {
-	assignToss(player1, player2);
-	throwDown(player1, player2);
+	assignToss(currentPlayerA, currentPlayerB);
+	throwDown(currentPlayerA, currentPlayerB);
+	updateScoreBoard(scoreBoard);
 	scoreKeeper();
-	winnerChecker(player1.wins, player2.wins);
+	winnerChecker(currentPlayerA.wins, currentPlayerB.wins);
 }
 
 function resetGame() {
 	player1.currentToss = null;
 	player2.currentToss = null;
+	player3.currentToss = null;
+	player4.currentToss = null;
 	player1.wins = 0;
 	player2.wins = 0;
+	player3.wins = 0;
+	player4.wins = 0;
 	console.groupEnd();
 	console.group('%cstart new round!', 'font-size: 14px; color: red;');
-	// console.countReset();
 	playButton.innerText = 'Play Round!';
 	playerThrows.innerText = 'Hit the button to Play the Game!!';
 	winnerText.innerText = '';
 	player1ScoreNumber.innerHTML = player1.wins;
 	player2ScoreNumber.innerHTML = player2.wins;
-	winner.forEach((element) => {
-		element.style.cssText = 'color: black';
-	});
+	// winner.forEach((element) => {
+	// 	element.style.cssText = 'color: black';
+	// });
+	winner.style.cssText = 'color: black';
 	playButton.onclick = gamePlayer;
 	// console.log(document.getElementById('score-row').children);
 	// .style = 'color: black';
