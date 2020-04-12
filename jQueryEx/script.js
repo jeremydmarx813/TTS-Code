4a92e295b42856c99329569452a95df10f7900b7
jeremydmarx <jeremydmarx@gmail.com>
2020-04-12 04:42:27 -0500

    completed requirements of form validation assignments

--- jQueryEx/script.js
+++ jQueryEx/script.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
-  $('#submit').click(function(){
-    var nameInput = $('#name');
-    var emailInput = $('#email');
-    var phoneInput = $('#phone');
-    var messageInput = $('#message');
-    var required = [nameInput, emailInput, phoneInput];
-
-    for(let i = 0; i < required.length; i++){
-        if(required[i].val() === ''){
-            required[i].val() = "Please Fill Out Required Fields";
-            required[i].addClass('warning');
+  var messagePStatus = false;
+  var messageP = $('#message-p');
+  var nameInput = $('#name');
+  var emailInput = $('#email');
+  var phoneInput = $('#phone');
+  var messageInput = $('#message-box');
+  var required = [ nameInput, emailInput, phoneInput ];
+	$('#submit').click(function() {
+		for (let i = 0; i < required.length; i++) {
+			if (required[i].val() === '') {
+				required[i].prev('label').addClass('warning');
+				if (messagePStatus === false) {
+					$(messageP).append(' <br> *"Please Fill Out Required Fields"*').addClass('warning');
+					messagePStatus = true;
         }
+			} else if (required[i].val() !== '') {
+        required[i].prev('label').removeClass('warning');
+			} 
     }
-  });
+    var labelArr = Array.from($('label')).every(function(e){
+       return e.className !== 'warning';
+     });
+
+     if(labelArr === true){
+       $('#form').remove();
+       $('#pre-form h2').text('Thanks for your feedback!');
+     }
+     
+    // if($('label'))
+	});
 });
