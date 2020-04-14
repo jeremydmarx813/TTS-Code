038069a478d26d9d16e34f3836552ed1c0efedd9
jeremydmarx <jeremydmarx@gmail.com>
2020-04-14 03:21:47 -0500

    cleaned up jquery syntax, func applying to minus buttons, text from input field not moving to newEl variable

--- toDoApp/jq.js
+++ toDoApp/jq.js
@@ -1,26 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	function minusButton() {
+		console.log('test');
+	}
 
-    function minusButton(event) {
-        event.click(function() {
-            console.log($(this).next('span'));
-            
-        })
-    }
+	Array.from($('#task-list').children()).forEach(function(e) {
+		$(e).children('button').click(minusButton);
+        
+	});
 
-    Array.from($('#task-list').children()).forEach(function(e){
-        //!!e.children() not working 
-        console.log(e);
-    });
-    // .click();
-
-    $('#task-add-button').click(function () {
-        if($('#task-to-add').val() !== ''){
-           
-            var newEl = `<li><button class="minus-button">-</button><span>${$('#task-to-add').val()}</span></li>`;
-            // newEl.children()[0].click(minusButton);
-            $('#task-list').append(newEl);
-            $('#task-to-add').val(' ');
-            // console.log($('#task-to-add'));
-        }
-    });
+	$('#task-add-button').click(function() {
+		if ($('#task-to-add').val() !== '') {
+            debugger; 
+			var newEl = `<li><button class="minus-button">-</button><span>${$('#task-to-add').val($(this))}</span></li>`;
+			$('#task-list').append($(newEl).children('button').click(minusButton.bind($(newEl).children('button'))));
+			$('#task-to-add').val(' ');
+		}
+	});
 });
