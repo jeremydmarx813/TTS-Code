533f9957f57867795fe6af54849475aca684681e
jeremydmarx <jeremydmarx@gmail.com>
2020-04-16 01:20:08 -0500

    fixed  minus button click working in addended list item

--- toDoApp/jq.js
+++ toDoApp/jq.js
@@ -5,19 +5,16 @@ $(document).ready(function() {
 
 	Array.from($('#task-list').children()).forEach(function(e) {
 		$(e).children('button').click(minusButton);
-        
 	});
 
 	$('#task-add-button').click(function() {
 		if ($('#task-to-add').val() !== '') {
-			// debugger; 
-			// var that = $(this).prev().children('input').val();
-			// console.log(that);
-			var newEl = `<li><button class="minus-button">-</button><span>${$('#task-to-add').val(  /*$(this).prev().children('input').val()*/)}</span></li>`;
-			$(newEl).children('button')  .click( function(){console.log('test')}/*  minusButton.bind($(newEl).children('button')) */ )   ;
-			// console.log($(newEl).children('button'));
-			$('#task-list').append(newEl /*  .children('button').click(  minusButton   .bind($(newEl).children('button'))  ) */ ) /* .children('button')  .click(minusButton)  */  ;
-			
+			// debugger;
+			var newEl = $(`<li><button class="minus-button">-</button><span>${$('#task-to-add').val()}</span></li>`);
+			$(newEl).children('button').click(minusButton);
+
+			$('#task-list').append(newEl);
+
 			$('#task-to-add').val('');
 		} else {
 			console.log('no input value');
