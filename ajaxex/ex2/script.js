000bb73edb649c1711dc335d8256adfdece34650
jeremydmarx <jeremydmarx@gmail.com>
2020-04-20 18:23:23 -0500

    started separating promises into indv funcs, started func to get user albums

--- ajaxex/ex2/script.js
+++ ajaxex/ex2/script.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 	var resultDisplay = $('#resultDisplay');
 
 	$(submitButton).click(function(event) {
-		event.preventDefault();
-
-		if ($(inputField).val()) {
-			return new Promise(function(pos, neg) {
-				$.get(`http://jsonplaceholder.typicode.com/users?username=${$(inputField).val()}`, function(results) {
+        event.preventDefault();
+        var nameFromInput = $(inputField).val();
+        var getUserFunc = function(n){
+            return new Promise(function(pos, neg) {
+				$.get(`http://jsonplaceholder.typicode.com/users?username=${n}`, function(results) {
 					//??How do i access status code on response obj
 					//?? Am I getting back json data?
 					if (results.length) {
@@ -20,32 +20,60 @@ $(document).ready(function() {
 						$(resultDisplay).append(newHTML);
 						pos(finalRes);
 					} else {
-						neg(`Username ${$(inputField).val()} not found`);
+						neg(`Username ${n} not found`);
 					}
 				});
 			})
-				.then(function(obj) {
-					return new Promise(function(pos, neg) {
-						$.get(`http://jsonplaceholder.typicode.com/posts?userId=${obj.id}`, function(posts) {
-							if (posts.length) {
-								console.log(posts);
-								var postsDiv = $(`<div><h3>User posts</h3><ul></ul></div>`);
-								posts.forEach(function(e) {
-									var newListItem = $(`<li>${e.title}</li>`);
-									$(newListItem).click(function() {
-										console.log('test');
-									});
-									$(postsDiv).children('ul').append(newListItem);
-								});
-								$(resultDisplay).append(postsDiv);
-								pos(posts);
-							} else {
-								neg(`User posts not found`);
-							}
-						});
-					});
-				}, console.log)
-				.catch(console.log);
+        };
+
+        var getUserPosts = function(obj) {
+            return new Promise(function(pos, neg) {
+                $.get(`http://jsonplaceholder.typicode.com/posts?userId=${obj.id}`, function(posts) {
+                    if (posts.length) {
+                        console.log(posts);
+                        var postsDiv = $(`<div><h3>User posts</h3><ul></ul></div>`);
+                        posts.forEach(function(e) {
+                            var newListItem = $(`<li>${e.title}</li>`);
+                            $(newListItem).click(function() {
+                                console.log('test');
+                            });
+                            $(postsDiv).children('ul').append(newListItem);
+                        });
+                        $(resultDisplay).append(postsDiv);
+                        pos(posts);
+                    } else {
+                        neg(`User posts not found`);
+                    }
+                });
+            });
+        };
+
+        var getUserAlbums = function(obj){
+            return new Promise(function(pos, neg) {
+                $.get(`http://jsonplaceholder.typicode.com/albums?userId=${obj.id}`, function(albums) {
+                    if (albums.length) {
+                        console.log(albums);
+                        var albumsDiv = $(`<div><h3>User Albums</h3><ul></ul></div>`);
+                        albums.forEach(function(e) {
+                            var newListItem = $(`<li>${e.title}</li>`);
+                            $(newListItem).click(function() {
+                                console.log('test');
+                            });
+                            $(albumsDiv).children('ul').append(newListItem);
+                        });
+                        $(resultDisplay).append(albusDiv);
+                        pos(albums);
+                    } else {
+                        neg(`User albums not found`);
+                    }
+                });
+            });
+        }
+
+		if ($(inputField).val()) {
+			
+                getUserFunc(nameFromInput).then(Promise.all([getUserPosts, getUserAlbums]))
+                .then(function(results){console.log(results)}, console.log);
 		} else {
 			console.error('no input entered');
 			$(resultDisplay).append(`<div><p>Error retrieving user</p></div>`);
