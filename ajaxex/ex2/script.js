802ce6707e8c6099470f1fb6112f3dce64fedf0d
jeremydmarx <jeremydmarx@gmail.com>
2020-04-20 16:15:53 -0500

    converted projected to Jquery syntax

--- ajaxex/ex2/script.js
+++ ajaxex/ex2/script.js
@@ -1,37 +1,31 @@
-var inputField = document.getElementById('userSubmit');
-var submitButton = document.getElementById('submitButton');
-var resultDisplay = document.getElementById('resultDisplay');
+$(document).ready(function() {
+	var inputField = $('#userSubmit');
+	var submitButton = $('#submitButton');
+	var resultDisplay = $('#resultDisplay');
 
-submitButton.addEventListener('click', function(event) {
-	event.preventDefault();
-	if (inputField.value) {
-		fetch('http://jsonplaceholder.typicode.com/users')
-			.then(function(response) {
-				return response.json();
-			})
-			.then(function(json) {
-				console.log(json);
-				var foundUser = json.find(function(obj) {
-					return obj.username === inputField.value;
+	$(submitButton).click(function(event) {
+		event.preventDefault();
+
+		if ($(inputField).val()) {
+			return new Promise(function(pos, neg) {
+				$.get(`http://jsonplaceholder.typicode.com/users?username=${$(inputField).val()}`, function(results) {
+					//??How do i access status code on response obj
+					if (results.length) {
+						pos(results[0]);
+					} else {
+						neg(`Username ${$(inputField).val()} not found`);
+					}
 				});
-				console.log(foundUser);
-
-				var foundUserPosts = fetch(`http://jsonplaceholder.typicode.com/posts?userId=${foundUser.id}`)
-					.then(function(response) {
-						return response.json();
-					})
-					.then(function(json) {
-                        console.log(json);
-                        json.forEach(function(e) {
-                            var nodeToAppend = document.createElement(`<div><p><span>Post number ${e.id}: </span><span>${e.title}</span></p></div>`);
-                            resultDisplay.appendChild(nodeToAppend);
-                        })
+			}).then(function(obj) {
+				return new Promise(function(pos, neg) {
+					$.get(`http://jsonplaceholder.typicode.com/posts?userId=${obj.id}`, function(posts) {
+						console.log(posts);
 					});
-
-				resultDisplay.innerHTML = `<div><p><span>User name: </span><span>${foundUser.name}</span></p></div>`;
-			});
-	} else {
-		console.error('no input entered');
-		resultDisplay.innerHTML = `<div><p>Error retrieving user</p></div>`;
-	}
+				});
+			}, console.log);
+		} else {
+			console.error('no input entered');
+			resultDisplay.append(`<div><p>Error retrieving user</p></div>`);
+		}
+	});
 });
