391b1beeee43bc8b5fb0c7329cd1e03c28f7c84b
jeremydmarx <jeremydmarx@gmail.com>
2020-04-20 16:48:43 -0500

    user full name and post title list generating to page, messing up layout of page, need to find a way to get posts and album in one promise

--- ajaxex/ex2/script.js
+++ ajaxex/ex2/script.js
@@ -10,22 +10,45 @@ $(document).ready(function() {
 			return new Promise(function(pos, neg) {
 				$.get(`http://jsonplaceholder.typicode.com/users?username=${$(inputField).val()}`, function(results) {
 					//??How do i access status code on response obj
+					//?? Am I getting back json data?
 					if (results.length) {
-						pos(results[0]);
+						var finalRes = results[0];
+						$(resultDisplay).empty();
+						var newHTML = $(
+							`<div><p><span class="data-field">Name of User:  </span><span>${finalRes.name}</span></p></div>`
+						);
+						$(resultDisplay).append(newHTML);
+						pos(finalRes);
 					} else {
 						neg(`Username ${$(inputField).val()} not found`);
 					}
 				});
-			}).then(function(obj) {
-				return new Promise(function(pos, neg) {
-					$.get(`http://jsonplaceholder.typicode.com/posts?userId=${obj.id}`, function(posts) {
-						console.log(posts);
+			})
+				.then(function(obj) {
+					return new Promise(function(pos, neg) {
+						$.get(`http://jsonplaceholder.typicode.com/posts?userId=${obj.id}`, function(posts) {
+							if (posts.length) {
+								console.log(posts);
+								var postsDiv = $(`<div><h3>User posts</h3><ul></ul></div>`);
+								posts.forEach(function(e) {
+									var newListItem = $(`<li>${e.title}</li>`);
+									$(newListItem).click(function() {
+										console.log('test');
+									});
+									$(postsDiv).children('ul').append(newListItem);
+								});
+								$(resultDisplay).append(postsDiv);
+								pos(posts);
+							} else {
+								neg(`User posts not found`);
+							}
+						});
 					});
-				});
-			}, console.log);
+				}, console.log)
+				.catch(console.log);
 		} else {
 			console.error('no input entered');
-			resultDisplay.append(`<div><p>Error retrieving user</p></div>`);
+			$(resultDisplay).append(`<div><p>Error retrieving user</p></div>`);
 		}
 	});
 });
