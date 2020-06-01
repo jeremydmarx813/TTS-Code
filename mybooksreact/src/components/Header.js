abfb04feb291e9f5d6a9fed3f9bcd23042eeae15
jeremydmarx <jeremydmarx@gmail.com>
2020-06-01 02:03:56 -0500

    added a few styles to project

--- mybooksreact/src/components/Header.js
+++ mybooksreact/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavbarToggler } from 'reactstrap';
+import { Navbar, Nav, NavItem } from 'reactstrap';
 
 const Header = () => {
 	return (
 		<div className="jumbotron text-center bg-info">
 			<h1>Bookshelves App</h1>
 			<Navbar light expand="md">
-				<NavItem>
-					<Link to="/">Home</Link>
-				</NavItem>
-				<NavItem>
-					<Link to="/saved-books">Saved Books</Link>
-				</NavItem>
+				<Nav className="mr-auto navbar">
+					<NavItem>
+						<Link to="/">Home</Link>
+					</NavItem>
+					<NavItem>
+						<Link to="/saved-books">Saved Books</Link>
+					</NavItem>
+				</Nav>
 			</Navbar>
 		</div>
 	);
